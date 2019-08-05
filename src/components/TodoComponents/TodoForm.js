df4530e2f6db55af67f4ce7bc6a637ad95f86a75
Reese Kunz <reesekunz21@gmail.com>
2019-08-05 14:35:11 -0700

    built out class layout for form

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,3 +1,25 @@
 // - `<TodoForm>` will hold your input field and your `Add Todo` and `Clear Completed` buttons.
 //   - Your input field should take in user input, and allow a user to press `Enter` or click on the `Submit Button` to add a todo to your list.
 //   - Once a todo is submitted, the Todo List should re-render and show the added todo.
+
+import React from "react";
+
+// building new class because form is going to manage its own state
+
+class TodoForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      
+    };
+  }
+  
+  render() {
+    return (
+
+    )
+  }
+
+
+
+}
