a96e5569a6928712bd6fa084193251a8553bf959
Reese Kunz <reesekunz21@gmail.com>
2019-08-05 12:26:04 -0700

    set up state layout

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
+// - `<App />` will hold all the data needed for this project. It will also be the container for your Todo Components.
+// - All of your application data will be stored here on `<App />`.
+// - All of your `handler` functions should live here on `<App />`.
+
 import React from 'react';
+import List from './components/TodoComponents/TodoList'
+
+const toDo = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
 
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor() {
+  super();
+  this.state = {
+  list: "hello from state"
+
+  }
+
+  }
   render() {
     return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
+      <div className="App">
+        <div className="Header">
+        <h2>To-do List:
+        </h2>
+        <List propsList= {this.state.list} />
       </div>
+      </div>
+
+
     );
   }
 }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,3 +1,7 @@
-// your components will all go in this `component` directory.
-// feel free to change this component.js into TodoList.js
+// - `<TodoList />` receives your Todos array and iterates over the list generating a new `<Todo />` for each element in the array.
 
+import React from 'react';
+
+const List = props => <div>This is the list: {props.propsList} </div>
+
+export default List;
